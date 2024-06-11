a52436520ff4c12d6e3d64471c563b04f31170b7
finnmyers96 <66837890+finnmyers96@users.noreply.github.com>
2024-06-11 10:04:24 -0400

    Add files via upload

--- getPolygons.py
+++ getPolygons.py
@@ -14,13 +14,32 @@ Dependencies:
     -- shapely (pip install shapely)
 """
 import geopandas as gpd
-from shapely.geomety import Polygon
+from shapely.geometry import Polygon
+from pyproj import Transformer
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path)
 
-def getPolygons(folderPath, sitenames):
+def transform_coordinates(easting, northing, from_epsg, to_epsg):
+    """
+    Transforms easting-northing coordinates to lon/lat
+    
+    Params:
+        easting (float, req): easting location
+        northing (float, req): northing location
+        from_epsg (int, req): EPSG code for spatial projection easting and 
+                              northing points are in
+        to_epsg (int, req): EPSG code to convert to for lat/lon
+        
+    Returns:
+        lon, lat (float): longitude and latitude of easting/northing coordinate
+    """
+    transformer = Transformer.from_crs(f'epsg:{from_epsg}', f'epsg:{to_epsg}', always_xy=True)
+    lon, lat = transformer.transform(easting, northing)
+    return lon, lat
+
+def getPolygons(folderPath, from_epsg, to_epsg):
     """
     Reads all shapefiles in a directory and stores their respective polygons
     in a dictionary
@@ -37,9 +56,11 @@ def getPolygons(folderPath, sitenames):
             gdf = gpd.read_file(fpath) # read shapefile
             for geom in gdf.geometry:
                 if isinstance(geom, Polygon):
-                    polygons[f'{f[:3]}'] = geom
+                    coords = geom.exterior.coords._coords[:, :2]
+                    lonlat_coords = [transform_coordinates(x, y, from_epsg, to_epsg) for x, y in coords]
+                    polygons[f'{f[:3]}'] = lonlat_coords
     return polygons
                     
-                    
-            
+polygon_dict = getPolygons(dir_path, 32145, 4326)
+
             
